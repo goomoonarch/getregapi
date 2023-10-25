e9b8ee1ce9fabcd2883120f79aa56d977e39d367
Jhoss <jhosser@live.com>
2023-10-25 15:24:53 -0500

    add some styles and text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 
 function Hero() {
-    return (
-        <div className='h-[100px] bg-gradient-to-r from-[#E6FAFE] to-[#EDDEFB]'>
-        </div>
-    );
+  return (
+    <div className="flex h-[150px] bg-gradient-to-r from-[#E6FAFE] to-[#EDDEFB] items-center justify-center px-6 pb-7 pt-3">
+        <h1 className="leading-[45px] bg-gradient-to-r from-[#BE76FE] to-[#4F6EFB] text-transparent bg-clip-text font-SFproDisplay text-[48px] space-x-[10px] text-start sm:text-center translate-x-9 sm:translate-x-0 sm:text-[60px]">Una alternativa rápida al ADRES.</h1>
+    </div>
+  );
 }
 
 export default Hero;
