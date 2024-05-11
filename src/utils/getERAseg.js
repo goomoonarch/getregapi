f30b8bb194520897e0517d39c0bbd9b6550d1829
Jhoss <jhosser@live.com>
2024-05-11 08:23:32 -0500

    change the logical of the fetching data for enable the WarningERaseg alert

--- src/utils/getERAseg.js
+++ src/utils/getERAseg.js
@@ -1,4 +1,31 @@
 export const getERAseg = async (dataResponse) => {
+  const edata = {
+    tipoDocumento: "CC",
+    numerodocumento: "1234567890",
+    primerApellido: "DOE",
+    segundoApellido: null,
+    primerNombre: "JANE",
+    segundoNombre: null,
+    regimenAfiliacion: null,
+    codigoRespuesta: "03",
+    codigoEPS: null,
+    nombreEPS: null,
+    fechaCorteSAT: null,
+    fechaConsulta: null,
+    fechaNacimiento: null,
+    sexo: null,
+    fechaCorteBDEX: null,
+    fechaCorteEvol: null,
+    estadoBDEX: null,
+    estadoEvol: null,
+    estadoAfiliacion: null,
+    estadoMIPRES: null,
+    fechaAfiliacionEntidad: null,
+    tipoAfiliado: null,
+    departamentoAfiliacion: null,
+    municipioAfiliacion: null,
+    codigoDivipola: null,
+  };
   const {
     tipoIdentificacion,
     numeroIdentificacion,
@@ -23,26 +50,20 @@ export const getERAseg = async (dataResponse) => {
     redirect: "follow",
   };
 
-  const response = await fetch(
-    //`http://localhost:3000/api/interoperabilidad/GetEPSPersonaMSS`,
-    "https://paiwebservices.paiweb.gov.co:8081/api/interoperabilidad/GetEPSPersonaMSS",
-    requestOptions
-  );
-  let data;
-  const status = response.status;
-  status === 200 ? (data = await response.json()) : null;
-
-  //---> Lógica de población del régimen especial o no asegurada! <---//
-  if (data.codigoRespuesta === "02") {
-    return {
-      data,
-      status,
-    };
-  //---> Lógica de población del régimen especial o no asegurada! <---//
-  } else {
-    return {
-      data,
-      status,
-    };
+  try {
+    const response = await fetch(
+      "https://paiwebservices.paiweb.gov.co:8081/api/interoperabilidad/GetEPSPersonaMSS",
+      requestOptions
+    );
+    let data;
+    const status = response.status;
+    status === 200 ? (data = await response.json()) : null;
+    if (data.codigoRespuesta === "02") {
+      return { data, status };
+    } else {
+      return { data, status };
+    }
+  } catch (e) {
+    if (e.message === "Failed to fetch") {return {data: edata, status: 606}}
   }
 };
