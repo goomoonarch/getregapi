4b46fe299a11764b3f585d96d9affe835e498abe
Jhoss <jhosser@live.com>
2024-05-18 16:52:18 -0500

    change the fortmat of the output by the copyFuntion

--- src/utils/birthFormatter.js
+++ src/utils/birthFormatter.js
@@ -1,7 +1,17 @@
-import { format } from 'date-fns';
-import { es } from 'date-fns/locale';
+import { format } from "date-fns";
+import { es } from "date-fns/locale";
 
 export const birthFormatter = (date) => {
-    const dateObj = new Date(date);
-    return format(dateObj, `dd 'de' MMMM 'de' yyyy`, { locale: es });
-}
+  const dateObj = new Date(date);
+  return format(dateObj, `dd 'de' MMMM 'de' yyyy`, { locale: es });
+};
+
+export const birthFormatter2 = (date) => {
+    const datetime = new Date(date);
+    
+    const day = String(datetime.getDate()).padStart(2, '0');
+    const month = String(datetime.getMonth() + 1).padStart(2, '0');
+    const year = datetime.getFullYear();
+
+    return `${day}/${month}/${year}`;
+};
--- src/utils/copyFunction.js
+++ src/utils/copyFunction.js
@@ -1,12 +1,34 @@
-import { birthFormatter } from "./birthFormatter";
+import { birthFormatter2 } from "./birthFormatter";
 
-export const copyFunction = ( userInfo ) => {
-    if (userInfo) {
-        const {dataResponse, ERAsegData, isERAsegReady, isReady} = userInfo
-        if ( !isReady || !isERAsegReady || !dataResponse ) {return}
-        const { tipoIdentificacion, numeroIdentificacion,primerApellido,segundoApellido, primerNombre, segundoNombre, fechaNacimiento, sexo } = dataResponse;
-        const { codigoEPS, estadoMIPRES, regimenAfiliacion, nombreEPS } = ERAsegData;
-        const copyInfo = `${tipoIdentificacion} ${numeroIdentificacion} ${primerApellido} ${segundoApellido ? segundoApellido : ''} ${primerNombre} ${segundoNombre ? segundoNombre : ''} ${birthFormatter(fechaNacimiento)} ${sexo} ${codigoEPS ? codigoEPS : ''} ${estadoMIPRES ? estadoMIPRES : ''} ${regimenAfiliacion ? regimenAfiliacion : ''} ${nombreEPS ? nombreEPS : ''}`;
-        navigator.clipboard.writeText(copyInfo);
+export const copyFunction = (userInfo) => {
+  if (userInfo) {
+    const { dataResponse, ERAsegData, isERAsegReady, isReady } = userInfo;
+    if (!isReady || !isERAsegReady || !dataResponse) {
+      return;
     }
-}
+    const {
+      tipoIdentificacion,
+      numeroIdentificacion,
+      primerApellido,
+      segundoApellido,
+      primerNombre,
+      segundoNombre,
+      fechaNacimiento,
+      sexo,
+    } = dataResponse;
+    const { codigoEPS, estadoMIPRES, regimenAfiliacion, nombreEPS } =
+      ERAsegData;
+    const copyInfo = `
+    Identificación: ${tipoIdentificacion} ${numeroIdentificacion}
+    Nombres: ${primerNombre} ${segundoNombre ? segundoNombre : ""}
+    Apellidos: ${primerApellido} ${segundoApellido ? segundoApellido : ""}
+    Fecha de nacimiento: ${birthFormatter2(fechaNacimiento)}
+    Sexo: ${sexo}
+    Código EPS: ${codigoEPS ? codigoEPS : ""}
+    Estado MIPRES: ${estadoMIPRES ? estadoMIPRES : ""}
+    Régimen de afiliación: ${regimenAfiliacion ? regimenAfiliacion : ""}
+    Nombre EPS: ${nombreEPS ? nombreEPS : ""}
+        `.trim();
+    navigator.clipboard.writeText(copyInfo);
+  }
+};
